6fe08479e2281e0d4911a374b928c5be72587dcc
Abdullahk70 <scientificgenius701924@gmail.com>
2024-12-30 13:08:40 +0500

    made changes in home screen links

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const SearchBar = () => {
   return (
     <div className="flex flex-col md:flex-row items-center justify-between bg-white rounded-lg shadow-lg p-4 space-y-4 md:space-y-0">
@@ -13,10 +13,12 @@ const SearchBar = () => {
       </div>
 
       {/* Upload Button */}
+      <Link to="/upload">
       <button className="px-6 py-2 bg-purple-600 text-white rounded-full flex items-center space-x-2 shadow-md hover:shadow-lg transition-shadow border border-gray-400">
         <span>Upload</span>
         <i className="fas fa-cloud-upload-alt"></i>
       </button>
+      </Link>
     </div>
   );
 };
